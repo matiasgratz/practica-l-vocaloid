02cbcc2fab5d2577fce6ab41ad8dd1404ea86990
matiasgratz <matiasgratz@frba.utn.edu.ar>
2021-12-05 11:32:23 -0300

    Llegué hasta acá error con doble negación

--- vocaloid.pl
+++ vocaloid.pl
@@ -8,12 +8,16 @@ cantante(megurineLuka,cancion(foreverYoung,5)).
 
 cantante(hatsuneMiku,cancion(tellYourWorld,5)).
 
-
 cantante(gumi,cancion(foreverYoung,4)).
 cantante(gumi,cancion(tellYourWorld,5)).
 
 cantante(seeU,cancion(novemberRain,6)).
 cantante(seeU,cancion(nightFever,4)).
+
+cantante(flojo,cancion(wasaa,2)).
+
+cantante(flojoYMuchosTemas,cancion(wasaa,2)).
+cantante(flojoYMuchosTemas,cancion(wasup,3)).
 
 % 1. Para comenzar el concierto, es preferible introducir primero a los cantantes más novedosos, por lo que necesitamos un predicado para saber si un vocaloid es novedoso cuando saben al menos 2 canciones y el tiempo total que duran todas las canciones debería ser menor a 15.
 
@@ -34,4 +38,11 @@ tiempoTotalMenorA15(Cantante):-
     
 % 2. Hay algunos vocaloids que simplemente no quieren cantar canciones largas porque no les gusta, es por eso que se pide saber si un cantante es acelerado, condición que se da cuando todas sus canciones duran 4 minutos o menos. Resolver sin usar forall/2.
 
- s(Cantante):-
+esAcelerado(Cantante):-
+    cantante(Cantante,_),
+    not(dura4OMas(Cantante)).
+
+dura4OMas(Cantante):-
+    cantante(Cantante,cancion(_,Duracion)),
+    not(Duracion >= 4).
+
